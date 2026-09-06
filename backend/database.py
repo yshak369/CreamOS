e9edfddaa76b86b4150215d807b46233e1dad638
yshak369 <168192798+yshak369@users.noreply.github.com>
2026-09-06 13:42:22 +0530

    Connect FastAPI to PostgreSQL

---
+++ backend/database.py
@@ -0,0 +1,15 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def get_connection():
+    return psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")
+    )
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from .database import get_connection
 
 app = FastAPI()
 
+
 @app.get("/")
-def root():
+def home():
     return {"message": "CreamOS API is running"}
+
+
+@app.get("/test-db")
+def test_db():
+    connection = get_connection()
+    connection.close()
+
+    return {"message": "PostgreSQL connection successful"}
